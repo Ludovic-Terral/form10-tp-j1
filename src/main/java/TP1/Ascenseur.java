b4f8878af5ebfb863dc1bc8edaa9f085eca2534f
UnConDePlus <UnConDePlus@172.16.238.184>
2023-09-29 16:27:39 +0200

    TP2-EX1

---
+++ src/main/java/TP1/Ascenseur.java
@@ -0,0 +1,52 @@
+package TP1;
+
+public class Ascenseur {
+
+	
+	private int _etageCurrent;
+	
+	public void setEtageInit(int etage) {
+		_etageCurrent = etage;
+	}
+	
+	public int getEtage() {
+		return _etageCurrent;
+	}
+	
+	
+	public void appelerDepuis(int etage) {
+		
+		System.out.print(">>Etage ");
+		System.out.print(etage);
+		System.out.println("<<");
+		
+		System.out.print("Etage courant: ");
+		System.out.println( getEtage());
+		
+		while(_etageCurrent != etage) {
+		
+			if ( _etageCurrent < etage) {
+			_etageCurrent++;
+			}
+			else {
+				_etageCurrent--;
+			}
+				
+			 System.out.print("etage courant: ");
+			 System.out.println( getEtage());
+		}
+	}
+	
+	
+    public static void main(String[] args) {
+    	
+    	Ascenseur A = new Ascenseur();
+    	A.setEtageInit(10);
+    	A.appelerDepuis(5);
+    	
+    }
+    
+    
+    
+	
+}
---
+++ src/main/java/TP1/BoiteDeVitesse.java
@@ -0,0 +1,25 @@
+package TP1;
+
+public class BoiteDeVitesse {
+	
+	private int _vitesseCurrent;
+	//Pour changer de vitesse
+	public void changerVitesse() {
+		
+	}
+	
+	//Pour passer à la vitesse inférieure
+	public void retrograder() {}
+
+
+
+	//Pour donner la vitesse courante (0 représente le point mort)
+	public int getVitesse() {
+		
+	}
+	
+    public static void main(String[] args) {
+    }
+	
+}
+
