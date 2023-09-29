187dc41d80ecfb26cc031b882de1fc3472af16cf
struixll <ileona-barbosa@hotmail.fr>
2023-09-29 11:01:13 +0200

    Premier commit

---
+++ src/main/java/fr/lixtec/form10/j1/exo/PremierPdf.java
@@ -0,0 +1,18 @@
+package fr.lixtec.form10.j1.exo;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class PremierPdf {
+
+  public static void main(String[] args) throws Exception {
+    Document document = new Document();
+    PdfWriter.getInstance(document, new FileOutputStream("premier-pdf.pdf"));
+    document.open();
+    document.addTitle("Mon premier PDF");
+    document.add(new Paragraph("Hello the world en PDF grâce à iText"));
+    document.close();
+  }
+}
