e1e5b734a3d760be47764b79fe806d1f4abcdc35
eizarn <66312384+eizarn@users.noreply.github.com>
2023-09-29 16:25:55 +0200

    auteur (test push)

--- src/main/java/fr/lixtec/form10/j1/tp/tp2/AlgoFrancois.java
+++ src/main/java/fr/lixtec/form10/j1/tp/tp2/AlgoFrancois.java
@@ -3,7 +3,7 @@ package fr.lixtec.form10.j1.tp.tp2;
 /**
  * Cette classe encode et décode selon la méthode de cryptage de Francois.
  * 
- * @author <a definir>
+ * @author Ewan IZARN
  */
 public class AlgoFrancois
 {
