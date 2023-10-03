921210ee38927b10599ca9519c67b0d4842757ba
Ronron <ronron1024@protonmail.com>
2023-10-03 20:51:55 +0200

    tp2 exo2

--- src/main/java/fr/lixtec/form10/j1/tp/tp2/AlgoFrancois.java
+++ src/main/java/fr/lixtec/form10/j1/tp/tp2/AlgoFrancois.java
@@ -1,68 +1,117 @@
 package fr.lixtec.form10.j1.tp.tp2;
 
-/**
- * Cette classe encode et décode selon la méthode de cryptage de Francois.
- * 
- * @author <a definir>
- */
-public class AlgoFrancois
-{
-    // ---------- static methods ----------
-    public static void main(String[] args)
-    {
-        AlgoFrancois algoFrancois = new AlgoFrancois();
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        String resultat = algoFrancois.encrypte("");
-        System.out.println("".equals(resultat));
+public class AlgoFrancois {
+	public class Message {
+		
+		private String decrypted;
+		private String encrypted;
+		
+		public Message(String decrypted, String encrypted) {
+			this.decrypted = decrypted;
+			this.encrypted = encrypted;
+		}
+		
+		public void verifyEncryption() {
+			System.out.println("""
+				Expected: %s
+				Found: %s
+				Passed: %s 
+			""".formatted(
+				this.encrypted,
+				encrypt(this.decrypted),
+				this.encrypted.equals(encrypt(this.decrypted))
+			));
+		}
+		
+		public void verifyDecryption() {
+			System.out.println("""
+				Expected: %s
+				Found: %s
+				Passed: %s 
+			""".formatted(
+				this.decrypted,
+				decrypt(this.encrypted),
+				this.decrypted.equals(decrypt(this.encrypted))
+			));
+		}
+		
+	}
+	
+	public static final int DEFAULT_SHIFT = 0;
+	public static final int N_ALPHA = 26; // number of character in the Latin alphabet
+	
+	private int shift;
+	
+	public AlgoFrancois(int shift) {
+		this.shift = shift;
+	}
+	
+	public AlgoFrancois() {
+		this(DEFAULT_SHIFT);
+	}
+	
+	public int getShift() {
+		return this.shift;
+	}
+	
+	public void setShift(int shift) {
+		this.shift = shift;
+	}
+	
+	public String encrypt(String message) {
+		String ret = "";
+		for (char l : message.toCharArray()) {
+			char toZero = Character.isLowerCase(l) ? 'a' : 'A';
+			ret += Character.isLetter(l)
+				? Character.toString((l - toZero + this.shift) % N_ALPHA + toZero)
+				: l
+			;
+		}
+		return ret;
+	}
+	
+	public String decrypt(String message) {
+		String ret = "";
+		for (char l : message.toCharArray()) {
+			char toZero = Character.isLowerCase(l) ? 'a' : 'A';
+			ret += Character.isLetter(l)
+				? Character.toString((l - toZero - this.shift + N_ALPHA) % N_ALPHA + toZero)
+				: l
+			;
+		}
+		return ret;
+	}
 
-        // étape 2
-        resultat = algoFrancois.encrypte("az");
-        System.out.println("Attendu: xw - Obtenu:"+resultat+" - Verifie:"+"xw".equals(resultat));
-
-        // étape 3
-        resultat = algoFrancois.encrypte("AZ");
-        System.out.println("Attendu: XW - Obtenu:"+resultat+" - Verifie:"+"XW".equals(resultat));
-
-        // étape 4
-        resultat = algoFrancois.encrypte("1,000.00");
-        System.out.println("Attendu: 1,000.00 - Obtenu:"+resultat+" - Verifie:"+"1,000.00".equals(resultat));
-
-        // étape 5
-        String phrase = "In cryptography, this cipher is one "
-                        + "of the simplest and most widely known "
-                        + "encryption techniques.";
-        resultat = algoFrancois.encrypte(phrase);
-
-        String phraseAttendue = "Fk zovmqldoxmev, qefp zfmebo fp lkb "
-                                + "lc qeb pfjmibpq xka jlpq tfabiv hkltk "
-                                + "bkzovmqflk qbzekfnrbp.";
-        System.out.println("Obtenu:"+resultat+" - Verifie:"+phraseAttendue.equals(resultat));
-        
-        //étape 6
-        resultat = algoFrancois.decrypte("xw");
-        System.out.println("Attendu: az - Obtenu:"+resultat+" - Verifie:"+"az".equals(resultat));
-        
-        //étape 7
-        resultat = algoFrancois.decrypte("XW");
-        System.out.println("Attendu: XW - Obtenu:"+resultat+" - Verifie:"+"AZ".equals(resultat));
-
-        //étape 8
-        resultat = algoFrancois.decrypte("Gb prfp xr ylrq ab jbp mbfkbp xsbz zbq xidl.");
-        System.out.println("Obtenu:"+resultat);        
-    }
-
-    
-
-    // ---------- instance methods ----------
-    public String encrypte(String texte)
-    {
-        return "";
-    }
-
-  
-    
-    public String decrypte(String texte)
-    {
-        return "";
-    }
+	public static void main(String args[]) {
+		final int shift = 23;
+		AlgoFrancois algoFrancois = new AlgoFrancois(shift);
+		
+		List<Message> encryptionTests = new ArrayList<>();
+		encryptionTests.add(algoFrancois.new Message("", ""));
+		encryptionTests.add(algoFrancois.new Message("az", "xw"));
+		encryptionTests.add(algoFrancois.new Message("1,000.00", "1,000.00"));
+		encryptionTests.add(algoFrancois.new Message("""
+			In cryptography, this cipher is one \
+			of the simplest and most widely known \
+			encryption techniques.""","""
+			Fk zovmqldoxmev, qefp zfmebo fp lkb \
+			lc qeb pfjmibpq xka jlpq tfabiv hkltk \
+			bkzovmqflk qbzekfnrbp."""));
+		
+		List<Message> decryptionTests = new ArrayList<>();
+		decryptionTests.add(algoFrancois.new Message("az", "xw"));
+		decryptionTests.add(algoFrancois.new Message("AZ", "XW"));
+		
+		for (Message test : encryptionTests)
+			test.verifyEncryption();
+		for (Message test : decryptionTests)
+			test.verifyDecryption();
+		
+		System.out.println(algoFrancois.decrypt("Gb prfp xr ylrq ab jbp mbfkbp xsbz zbq xidl."));
+	}
+	
 }
