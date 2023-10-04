9b00baaec6cb478f576006a89e59de5cd4b0f1ab
Thomas <mailrt83-github@yahoo.com>
2023-10-04 13:46:04 +0200

    Ascenseur TP1 exo1

--- src/main/java/fr/lixtec/form10/j1/tp/tp1/Ascenseur.java
+++ src/main/java/fr/lixtec/form10/j1/tp/tp1/Ascenseur.java
@@ -1,6 +1,25 @@
 package fr.lixtec.form10.j1.tp.tp1;
 
 public class Ascenseur {
-	
-	
+    private int etageActuel = 0;
+
+    public int getEtage() {
+        return etageActuel;
+    }
+
+    public void EtageDemander(int etage) {
+        System.out.println("Du " + etageActuel + " au " + etage);
+        etageActuel = etage;
+    }
+
+    public static void main(String[] args) {
+        Ascenseur ascenseur = new Ascenseur();
+
+        System.out.println("Etage Actuelle " + ascenseur.getEtage());
+
+        ascenseur.EtageDemander(5);
+        
+        System.out.println("Il est au " + ascenseur.getEtage());
+
+    }
 }
