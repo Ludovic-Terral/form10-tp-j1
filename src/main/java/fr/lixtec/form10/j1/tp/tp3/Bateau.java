d9c71d0e89f1b4d45ba965205e062db61c8ce611
Thomas <mailrt83-github@yahoo.com>
2023-10-14 01:19:56 +0200

    Modification of Grille.java

--- src/main/java/fr/lixtec/form10/j1/tp/tp3/Bateau.java
+++ src/main/java/fr/lixtec/form10/j1/tp/tp3/Bateau.java
@@ -1,5 +1,41 @@
 package fr.lixtec.form10.j1.tp.tp3;
 
 public class Bateau {
+    private String nom;
+    private int taille;
+    private char orientation;
+    private int ligne;
+    private int colonne;
 
+    public Bateau(String nom, int taille, char orientation) {
+        this.nom = nom;
+        this.taille = taille;
+        this.orientation = orientation;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public char getOrientation() {
+        return orientation;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+
+    public void setCoordonnees(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
+    }
 }
+
--- src/main/java/fr/lixtec/form10/j1/tp/tp3/Grille.java
+++ src/main/java/fr/lixtec/form10/j1/tp/tp3/Grille.java
@@ -1,48 +1,107 @@
 package fr.lixtec.form10.j1.tp.tp3;
 
 public class Grille {
-	
-	// Attributs
-	private int NbLigne;
-	private int NbColonne;
-	private char [][] grille;
-	
-	//Constructeur
-	public Grille(int l, int c) {
-		NbLigne = l;
-		NbColonne = c;
-		grille = new char[NbLigne][NbColonne];
-		
-		for(int i=0; i<NbLigne; i++){
-			for(int j=0; j<NbColonne; j++) {
-				
-				grille[i][j] = '-';
-				
-			}
-		}
-	}
+    private static final int TAILLE_GRILLE_LIGNE = 10;
+    private static final int TAILLE_GRILLE_COLONNE = 8;
+    private static final char CASE_VIDE = ' ';
 
-	public void afficher() {
-		System.out.println();
-		for(int i=0; i<NbLigne; i++){
-			for(int j=0; j<NbColonne; j++) {
-				
-				 System.out.print(" |"+ grille[i][j]);
-				
-			}
-			
-			System.out.println(" | ");
-		}
+    private char[][] grille;
+
+    public Grille() {
+        this.grille = new char[TAILLE_GRILLE_LIGNE][TAILLE_GRILLE_COLONNE];
+        initialiserGrille();
+    }
+
+    private void initialiserGrille() {
+        // Initialiser la grille avec des cases vides
+        for (int i = 0; i < TAILLE_GRILLE_LIGNE; i++) {
+            for (int j = 0; j < TAILLE_GRILLE_COLONNE; j++) {
+                grille[i][j] = CASE_VIDE;
+            }
+        }
+    }
+
+    public void afficher() {
+        System.out.print("    ");
+        for (int i = 0; i < TAILLE_GRILLE_COLONNE; i++) {
+            System.out.print("  " + (char) ('A' + i) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < TAILLE_GRILLE_LIGNE; i++) {
+            System.out.print(" ");
+            int numLigne = i + 1;
+            if (numLigne < 10) {
+                System.out.print(" ");
+            }
+            System.out.print(numLigne + " |");
+
+            for (int j = 0; j < TAILLE_GRILLE_COLONNE; j++) {
+                System.out.print(" " + grille[i][j] + " |");
+            }
+            System.out.println();
+            System.out.println("    +---+---+---+---+---+---+---+---+");
+        }
+        System.out.println();
+    }
+
+
+
+
+
+    public void placerBateau(Bateau bateau) {
+        // Assurez-vous que les coordonnées du bateau sont valides
+        int ligne = bateau.getLigne();
+        int colonne = bateau.getColonne();
+        int taille = bateau.getTaille();
+        char orientation = bateau.getOrientation();
+
+        if (ligne < 1 || ligne > TAILLE_GRILLE_LIGNE || colonne < 1 || colonne > TAILLE_GRILLE_COLONNE) {
+            System.out.println("Coordonnées invalides pour le placement du bateau.");
+            return;
+        }
+
+        // Vérifiez que le placement est possible
+        if (orientation == 'H') {
+            if (colonne + taille - 1 > TAILLE_GRILLE_COLONNE) {
+                System.out.println("Placement impossible pour le bateau.");
+                return;
+            }
+            for (int j = colonne - 1; j < colonne + taille - 1; j++) {
+                if (grille[ligne - 1][j] != CASE_VIDE) {
+                    System.out.println("Case déjà occupée.");
+                    return;
+                }
+            }
+            // Placez le bateau
+            for (int j = colonne - 1; j < colonne + taille - 1; j++) {
+                grille[ligne - 1][j] = 'B'; // 'B' pour représenter le bateau
+            }
+        } else if (orientation == 'V') {
+            if (ligne + taille - 1 > TAILLE_GRILLE_LIGNE) {
+                System.out.println("Placement impossible pour le bateau.");
+                return;
+            }
+            for (int i = ligne - 1; i < ligne + taille - 1; i++) {
+                if (grille[i][colonne - 1] != CASE_VIDE) {
+                    System.out.println("Case déjà occupée.");
+                    return;
+                }
+            }
+            // Placez le bateau
+            for (int i = ligne - 1; i < ligne + taille - 1; i++) {
+                grille[i][colonne - 1] = 'B'; // 'B' pour représenter le bateau
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
 		
-		System.out.println();
-	}
-	
-	public static void main(String[] args) {
-		
-		Grille g1 = new Grille(8,10);
+		Grille g1 = new Grille();
 		g1.afficher();
 		System.out.println();
-		Grille g2 = new Grille(8,10);
+		Grille g2 = new Grille();
 		g2.afficher();
 	}
+
 }
